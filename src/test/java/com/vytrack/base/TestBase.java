c262dac15f657499dd8c2d2b0c8ac0efacff97b3
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-06-11 18:37:54 -0400

    Victoria created private variables with login and password values for all users in the TestBase class

--- src/test/java/com/vytrack/base/TestBase.java
+++ src/test/java/com/vytrack/base/TestBase.java
@@ -8,8 +8,19 @@ import org.testng.annotations.BeforeMethod;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
-
     public WebDriver driver;
+
+    private final String TRUCK_DRIVER_1_LOGIN = "user42";
+    private final String TRUCK_DRIVER_2_LOGIN = "user43";
+    private final String TRUCK_DRIVER_3_LOGIN = "user44";
+    private final String STORE_MANAGER_1_LOGIN = "storemanager79";
+    private final String STORE_MANAGER_2_LOGIN = "storemanager80";
+    private final String STORE_MANAGER_3_LOGIN = "storemanager247";
+    private final String SALES_MANAGER_1_LOGIN = "salesmanager143";
+    private final String SALES_MANAGER_2_LOGIN = "salesmanager144";
+    private final String SALES_MANAGER_3_LOGIN = "salesmanager145";
+
+    private final String PASSWORD = "UserUser123";
 
     @BeforeMethod
     public void setup(){
@@ -22,5 +33,45 @@ public class TestBase {
     @AfterTest
     public void tearDown(){
         driver.quit();
+    }
+
+    public String getTRUCK_DRIVER_1_LOGIN() {
+        return TRUCK_DRIVER_1_LOGIN;
+    }
+
+    public String getTRUCK_DRIVER_2_LOGIN() {
+        return TRUCK_DRIVER_2_LOGIN;
+    }
+
+    public String getTRUCK_DRIVER_3_LOGIN() {
+        return TRUCK_DRIVER_3_LOGIN;
+    }
+
+    public String getSTORE_MANAGER_1_LOGIN() {
+        return STORE_MANAGER_1_LOGIN;
+    }
+
+    public String getSTORE_MANAGER_2_LOGIN() {
+        return STORE_MANAGER_2_LOGIN;
+    }
+
+    public String getSTORE_MANAGER_3_LOGIN() {
+        return STORE_MANAGER_3_LOGIN;
+    }
+
+    public String getSALES_MANAGER_1_LOGIN() {
+        return SALES_MANAGER_1_LOGIN;
+    }
+
+    public String getSALES_MANAGER_2_LOGIN() {
+        return SALES_MANAGER_2_LOGIN;
+    }
+
+    public String getSALES_MANAGER_3_LOGIN() {
+        return SALES_MANAGER_3_LOGIN;
+    }
+
+    public String getPASSWORD() {
+        return PASSWORD;
     }
 }
