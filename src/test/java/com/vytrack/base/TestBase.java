5fb4a7a8cdd722b3815449a98468fa291324f190
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-06-11 21:32:04 -0400

    class TestBase changed

--- src/test/java/com/vytrack/base/TestBase.java
+++ src/test/java/com/vytrack/base/TestBase.java
@@ -2,6 +2,7 @@ package com.vytrack.base;
 
 import com.vytrack.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 
@@ -9,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     public WebDriver driver;
+    public WebDriverWait wait;
 
     private final String TRUCK_DRIVER_1_LOGIN = "user42";
     private final String TRUCK_DRIVER_2_LOGIN = "user43";
@@ -28,6 +30,7 @@ public class TestBase {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterTest
